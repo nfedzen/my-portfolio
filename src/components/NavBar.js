38c7e722300067a108d0f790e0040dc3f7102ec5
Nathan Fedzen <nfedzen@hotmail.com>
2021-02-25 17:46:56 -0700

    styling complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,38 +5,38 @@ import { SocialIcon } from "react-social-icons"
 
 export default function NavBar(){
   return (
-    <header className="bg-red-600">
+    <header className="bg-gray-900">
       <div className="container mx-auto flex justify-between">
         <nav className="flex">
           <NavLink 
             to="/"
             exact
-            activeClassName="text-white"
-            className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+            activeClassName="text-gray-400"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-gray-100 hover:text-gray-600 text-4xl font-bold cursive tracking-widest"
           >
-            Nathan
+            Fedzen
           </NavLink>
 
           <NavLink 
             to="/post" 
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-100 hover:text-gray-600"
+            activeClassName="text-gray-400 bg-gray-700"
           >
             Blog Posts
           </NavLink>
 
           <NavLink 
             to="/project" 
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-100 hover:text-gray-600"
+            activeClassName="text-gray-400 bg-gray-700"
           >
             Projects
           </NavLink>
 
           <NavLink 
             to="/about" 
-            className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
-            activeClassName="text-red-100 bg-red-700"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-gray-100 hover:text-gray-600"
+            activeClassName="text-gray-400 bg-gray-700"
           > 
             About Me!
           </NavLink>
--- src/components/Post.js
+++ src/components/Post.js
@@ -23,10 +23,10 @@ export default function Post(){
   }, []);
 
   return (
-    <main className="bg-green-100 min-h-screen p-12">
+    <main className="bg-gray-900 min-h-screen p-12">
       <section className="container mx-auto">
-        <h1 className="text-5xl flex justify-center cursive">Blog Posts Page</h1>
-        <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my page of blog posts</h2>
+        <h1 className="text-5xl text-gray-400 flex justify-center cursive">Blog Posts Page</h1>
+        <h2 className="text-lg text-gray-100 flex justify-center mb-12">Welcome to my page of blog posts</h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {postData && postData.map((post,index) =>(
             <article>
